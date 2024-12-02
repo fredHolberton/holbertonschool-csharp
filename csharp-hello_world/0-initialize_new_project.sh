64dabe5d901abd4b8e98223f64e63720b2f5959d
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-02 13:07:39 +0100

    task 0 Fix 4

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 mkdir 0-new_project
-mkdir ./0-new_project/obj
-touch ./0-new_project/0-new_project.csproj
-touch ./0-new_project/Program.cs
+cd ./0-new_project
+dotnet new console
 
