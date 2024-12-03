5f6ea97c857e183dda3c18ee1abb0cd3f3b41d67
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-03 14:24:11 +0100

    Task 3 Fix 4

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Programming is like building a multilingual puzzle");
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("\"Programming is like building a multilingual puzzle");
+	}
+}
