2d17b1ba4d1ba4d7f498cf71faabcc856ecf7bd6
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-03 16:36:57 +0100

    9. Print specifiers

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0}%\nCurrency: ${1}", percent * 100, currency.ToString("#,###.00", CultureInfo.CreateSpecificCulture("en-US.utf-8")));
+	}
+}
