ec0ebbdf477c93ba73c81bb40c611f27523b7d9a
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-04 17:00:48 +0100

    task 9 Fix 4

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0}\nCurrency: ${1}", percent.ToString("P2",  new System.Globalization.CultureInfo("en-US")), currency.ToString("N2", new System.Globalization.CultureInfo("en-US")));
+		Console.WriteLine("Percent: {0}\nCurrency: {1}", percent.ToString("P2",  new System.Globalization.CultureInfo("en-US")), currency.ToString("C2", new System.Globalization.CultureInfo("en-US")));
 	}
 }
