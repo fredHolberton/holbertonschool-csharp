3861106fe83545479346865c05e86571de100244
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-04 11:11:19 +0100

    5. 00...99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i < 99)
+            {
+                Console.Write("{0}, ", i.ToString("00"));
+            }
+            else
+            {
+                Console.Write("{0}", i.ToString("00"));
+            }
+        }
+    }
+}
