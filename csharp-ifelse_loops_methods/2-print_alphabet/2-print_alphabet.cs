335cd259fca52c5e59abb7a1ffb5e8e8edc91b5a
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-04 10:46:08 +0100

    2. I sometimes suffer from insomnia. And...

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string alphabet = string.Empty;
+        for (int i = 0; i < 26; i++)
+        {
+            alphabet = String.Concat(alphabet, Convert.ToChar(i + 97));
+        }
+        Console.Write(alphabet);
+    }
+}
