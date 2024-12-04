ecdfa6d77d50653ec944f71c479d8615bdcaa555
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-04 12:21:28 +0100

    8. There are only 3 colors, 10 digits, and 7 notes

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = Math.Abs(number) % 10;
+        if (number < 0 ) 
+        {
+            lastDigit = lastDigit * -1;
+        }
+        return lastDigit;
+    }
+}
