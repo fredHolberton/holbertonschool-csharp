2d50c89d5ab2c3e7961b34baece1e87e36500c52
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-04 11:26:57 +0100

    6. Inventing is a combination of brains...

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                if (i < 8)
+                {
+                    Console.Write("{0}{1}, ", i, j);
+                }
+                else
+                {
+                    Console.Write("{0}{1}\n", i, j);
+                }        
+            }
+        }
+    }
+}
