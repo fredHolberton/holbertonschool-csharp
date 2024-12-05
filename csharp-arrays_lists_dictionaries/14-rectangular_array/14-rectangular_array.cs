f47aca7dca937bf8d33761c03da432b06f9e6547
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-05 15:26:30 +0100

    14. Rectangular array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5,5];
+
+        // initialize array
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == 2 && j == 2)
+                    array[i,j] = 1;
+                else
+                    array[i,j] = 0;
+            }
+       }
+
+        // print array
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (j < 4)
+                    Console.Write("{0} ", array[i, j]);
+                else
+                    Console.Write("{0}", array[i, j]);
+            }
+             Console.Write("\n");
+        }
+    }
+}
