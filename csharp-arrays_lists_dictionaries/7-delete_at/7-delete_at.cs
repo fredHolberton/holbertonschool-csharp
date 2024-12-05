8e8e1ac626072de63660ceb5c950d55d4f0d419c
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-05 10:47:36 +0100

    7. Delete at

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        // cas d'erreur : L'index est négatif ou supérieur au nombre d'éléments de la liste
+        if (index <= 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i == index)
+            {
+                myList.Remove(myList[i]);
+                i++;
+            }
+        }
+        return myList;
+    }
+}
