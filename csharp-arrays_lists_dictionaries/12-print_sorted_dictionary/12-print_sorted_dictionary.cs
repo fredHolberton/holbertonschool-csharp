7e43efa8630e34a3bbe5b255649361bb8d5b6b9e
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-05 13:55:44 +0100

    12. Print sorted dictionary

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> sortedList = new List<string>();
+
+        // créer une liste avec les key du dictionnaire
+        foreach (KeyValuePair<string, string> kvp in myDict)
+            sortedList.Add(kvp.Key);
+        
+        // trier la liste
+        sortedList.Sort();
+
+        foreach (string elt in sortedList)
+            Console.WriteLine("{0}: {1}", elt, myDict[elt]);
+    }
+}
