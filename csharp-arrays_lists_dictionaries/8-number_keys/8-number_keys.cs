c3862ee622255016524a8df1e9f83b82943c63e3
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-05 11:19:19 +0100

    8. Number of keys

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int countElt = 0;
+        foreach (KeyValuePair<string, string> kvp in myDict)
+            countElt++;
+        return countElt;
+    }
+}
