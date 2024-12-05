b8d56be72dc80a3b8da51834b15246e979a4c11a
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-05 11:50:19 +0100

    Task 9 Fix 1

--- csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,7 +5,7 @@ class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
-        if (!myDict.ContainsKey(key))
+        if (myDict.ContainsKey(key))
             myDict[key] = value;
         else
             myDict.Add(key, value);
