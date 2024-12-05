fb360f649e664874c6762bd11b25b94877b0b3af
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-05 14:54:56 +0100

    13. Best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int maxInt = -1;
+
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (kvp.Value > maxInt)
+            maxInt = kvp.Value;
+        }
+        if (maxInt == -1)
+            return "None";
+        else
+            return maxInt.ToString();
+    }
+}
