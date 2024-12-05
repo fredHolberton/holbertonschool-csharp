0d8e198954934ebb64068f7e10abc8598011cce3
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-05 15:07:03 +0100

    Task 13 Fix 1

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,15 +6,20 @@ class Dictionary
     public static string BestScore(Dictionary<string, int> myList)
     {
         int maxInt = -1;
+        string bestScore = String.Empty;
 
         foreach (KeyValuePair<string, int> kvp in myList)
         {
             if (kvp.Value > maxInt)
-            maxInt = kvp.Value;
+            {
+                maxInt = kvp.Value;
+                bestScore = kvp.Key;
+            }
+            
         }
         if (maxInt == -1)
             return "None";
         else
-            return maxInt.ToString();
+            return bestScore;
     }
 }
