f33ba1cb39300c0f766de8e3feb745029d98c7e8
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 11:18:54 +0100

    4. Queue it up

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int numberOfItems = aQueue.Count;
+        Console.WriteLine("Number of items: {0}", numberOfItems);
+        if (numberOfItems > 0)
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+
+        while (aQueue.Contains(search))
+                aQueue.Dequeue();
+        
+        return aQueue;
+
+    }
+}
