7b54b16caee82f0dcb13bb53b68f26c54d7c140b
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 14:59:43 +0100

    Task 9 Fix 1

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -6,8 +6,8 @@ class LList
    public static int Pop(LinkedList<int> myLList)
    {   
         int firstNodeValue = 0;
-        LinkedListNode<int>? firstNode;
-        if (myLList.Count > 0)
+        LinkedListNode<int> firstNode;
+        if (myLList.First != null)
         {
             firstNode = myLList.First;
             firstNodeValue = firstNode.Value;
