5e9badb1a25d3267688f17ab4e33ecde6163e1c3
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 12:22:37 +0100

    6. LinkedList length

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static int Length(LinkedList<int> myLList)
+   {
+        int numberOfItem = 0;
+
+        for (LinkedListNode<int> node = myLList.First; node != null; node=node.Next)
+        {
+            numberOfItem++;
+        }
+        
+        return numberOfItem;
+   } 
+}
