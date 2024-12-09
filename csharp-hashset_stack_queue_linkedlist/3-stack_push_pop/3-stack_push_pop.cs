cf54e5e011d813d1ef333d9a2e8b14d329f88678
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 10:46:32 +0100

    3. Full stack

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int numberOfItems = aStack.Count;
+        Console.WriteLine("Number of items: {0}", numberOfItems);
+        if (aStack.Count > 0)
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        else
+            Console.WriteLine("Stack is empty");
+
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Contains(search))
+                aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+        return aStack;
+
+    }
+}
