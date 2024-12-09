6039b671c32bffcdcc552675bcbb48351b89b237
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 15:26:46 +0100

    10. Get node at index

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static int GetNode(LinkedList<int> myLList, int n)
+   {
+        
+        int searchedItem = 0;
+        int i = -1;
+        foreach (int item in myLList)
+        {
+            i++;
+            if (i == n)
+            {
+                searchedItem = item;
+                break;
+            }           
+        }
+              
+        return searchedItem;
+   } 
+}
