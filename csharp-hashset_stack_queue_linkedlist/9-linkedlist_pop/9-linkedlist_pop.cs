692fd63c3faa7799595bc426e5bf37a2b7d90058
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 14:50:31 +0100

    9. Pop

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static int Pop(LinkedList<int> myLList)
+   {   
+        int firstNodeValue = 0;
+        LinkedListNode<int>? firstNode;
+        if (myLList.Count > 0)
+        {
+            firstNode = myLList.First;
+            firstNodeValue = firstNode.Value;
+            myLList.RemoveFirst();
+        }
+                             
+        return firstNodeValue;
+   } 
+}
