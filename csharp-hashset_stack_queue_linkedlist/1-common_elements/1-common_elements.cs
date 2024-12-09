67a7637705cf53fef132f82edbb9c3ce3c7a067b
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 09:47:28 +0100

    1. Present in both

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonList = new List<int>();
+
+        foreach (int lst in list1)
+        {
+            if (list2.Contains(lst))
+            {
+                commonList.Add(lst);
+            }
+        }
+
+        return commonList;
+    }
+}
