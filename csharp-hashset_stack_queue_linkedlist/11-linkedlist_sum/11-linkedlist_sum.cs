63143130ee3b43abad8ef89398df9aeb697e6d77
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 15:38:42 +0100

    11. LinkedList sum

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static int Sum(LinkedList<int> myLList)
+   {
+        
+        int sumItem = 0;
+        foreach (int item in myLList)
+        {
+            sumItem+=item;         
+        }
+              
+        return sumItem;
+   } 
+}
