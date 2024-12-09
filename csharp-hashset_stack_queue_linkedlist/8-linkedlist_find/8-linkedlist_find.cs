f5de2e88b222cd7516748a9cb3be77bc5a2010f1
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 14:19:29 +0100

    8. Find value

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static int FindNode(LinkedList<int> myLList, int value)
+   {
+        int foundItem = -1;
+        int i = -1;
+        foreach (int item in myLList)
+        {
+            i++;
+            if (item == value)
+            {
+                foundItem = i;
+                break;
+            }           
+        }
+              
+        return foundItem;
+   } 
+}
