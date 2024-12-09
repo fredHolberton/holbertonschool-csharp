4ef8c3f0411e9431fff770b7663c6e2709137dfd
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-09 17:04:32 +0100

    13. Delete at index

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static void Delete(LinkedList<int> myLList, int index)
+   {
+        
+        int searchedItem = 0;
+        int i = -1;
+        foreach (int item in myLList)
+        {
+            i++;
+            if (i == index)
+            {
+                myLList.Remove(item);
+                break;
+            }           
+        } 
+   } 
+}
