6eb6d6c457011884bd05307520b572ae1585583b
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-12 10:23:30 +0100

    task 16 fix 9

--- csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -4,17 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[][] jaggedArray = new int[3][];
-
-        jaggedArray[0] = {0, 1, 2, 3};
-        jaggedArray[1] = {0, 1, 2, 3, 4, 5, 6};
-        jaggedArray[2] = {0, 1};
-
+        int[][] jaggedArray =  {{0 1 2 3}, {0 1 2 3 4 5 6}, {0 1}};
+        
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < jaggedArray[i].Length; j++)
             {
-                Console.Write("{0}{1}", jaggedArray[i][j], j < (jaggedArray[i].Length - 1) ? "*" : "");
+                Console.Write("{0}{1}", jaggedArray[i][j], j < (jaggedArray[i].Length - 1) ? " " : "");
             }
             Console.WriteLine();
         }
