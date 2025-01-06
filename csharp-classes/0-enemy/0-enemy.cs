19f071f55399e10fece51c5d5a08012f55d99db7
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-06 10:34:31 +0100

    0. Braaainsss

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This is an empty public class within Enemies namespace.</summary>
+    public class Zombie
+    {
+        
+    }
+}
---
+++ csharp-classes/README.md
@@ -0,0 +1,1 @@
+Contains all task file of project : C# - Classes and Namespaces
