78f2ca687ebeb641a0a659709d9141ad68db93b9
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-06 13:28:37 +0100

    5. Printing a zombie

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This is an public class within Enemies namespace.</summary>
+    public class Zombie
+    {
+        /// Private field for Zombie.health.
+        private int health;
+        /// Private field for Zombie.name
+        private string name = "(No name)";
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class with initial value for health.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        /// <summary>Gets or sets the name of the Zombie.</summary>
+        public string Name 
+        { 
+            get
+            {
+                return name;
+            } 
+            set
+            {
+                if (value == string.Empty)
+                {
+                    this.name = "(No name)";
+                }
+                else
+                {
+                    name = value;
+                }
+                
+            }
+        }
+
+        /// <summary>Function that Return health value.</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        /// <summary>Override that prints the Zombie object’s attributes to stdout</summary>
+        public override string ToString()
+        {
+            return "Zombie Name: " + Name + " / Total Health: " + this.health;
+        }
+    }
+}
