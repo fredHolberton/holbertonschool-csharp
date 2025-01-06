0e95dfcadf782ef6a8b0dc8c7cb1ca20231de906
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-06 10:56:53 +0100

    1. Healthy competition

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This is an public class within Enemies namespace.</summary>
+    public class Zombie
+    {
+        /// <summary>Public field for Zombie.health.</summary>
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
