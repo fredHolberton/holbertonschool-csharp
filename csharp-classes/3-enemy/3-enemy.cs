29a8dae41b753c3172f90356f5bc6bffc0d8dd7f
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-06 11:23:05 +0100

    3. Zombie health

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This is an public class within Enemies namespace.</summary>
+    public class Zombie
+    {
+        /// private field for Zombie.health.
+        private int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class with initial value for health.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        /// <summary>Function that Return health value.</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
