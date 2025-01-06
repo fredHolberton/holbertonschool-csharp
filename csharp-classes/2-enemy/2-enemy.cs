ff0f8e0c4eebc47e138a7afa8846bd36c7c054f3
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-06 11:07:41 +0100

    2. Health validation

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This is an public class within Enemies namespace.</summary>
+    public class Zombie
+    {
+        /// <summary>Public field for Zombie.health.</summary>
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// <param name="value">The value of the health of the Zombie.</param>
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+    }
+}
