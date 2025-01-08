6774ab6b894b5fa332a7eb6f758045c74c9b2431
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-08 09:16:37 +0100

    1. Divide a matrix

---
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,67 @@
+namespace MyMath.Tests
+{
+    /// <summary>Class for testing Matrix.Divide function.</summary>
+    [TestFixture]
+    public class DivideTests
+    {
+        /// <summary>Test when matrix is not null and n not zero.</summary>
+        [Test]
+        public void TestMatrixNonNullEtNNotZero()
+        {
+            // Arrange
+            int[,] maMatrix = new int[3, 3] { { 10, 15, 20 }, { 20, 30, 40 }, { 30, 60, 80 } };
+            int n = 5;
+
+            // Act
+            int[,] output = Matrix.Divide(maMatrix, n);
+
+            // Assert
+            Assert.AreEqual(new int[3, 3] { { 2, 3, 4 }, { 4, 6, 8 }, { 6, 12, 16 } }, output);
+        }
+
+        /// <summary>Test when matrix is null and n not zero.</summary>
+        [Test]
+        public void TestMatrixIsNullEtNNotZero()
+        {
+            // Arrange
+            int[,] maMatrix = null;
+            int n = 5;
+
+            // Act
+            int[,] output = Matrix.Divide(maMatrix, n);
+
+            // Assert
+            Assert.AreEqual(null, output);
+        }
+
+        /// <summary>Test when matrix not null and n is zero.</summary>
+        [Test]
+        public void TestMatrixNotNullEtNIsZero()
+        {
+            // Arrange
+            int[,] maMatrix = new int[3, 3] { { 10, 15, 20 }, { 20, 30, 40 }, { 30, 60, 80 } };
+            int n = 0;
+
+            // Act
+            int[,] output = Matrix.Divide(maMatrix, n);
+
+            // Assert
+            Assert.AreEqual(null, output);
+        }
+
+        /// <summary>Test when matrix is null and n is zero.</summary>
+        [Test]
+        public void TestMatrixIsNullEtNIsZero()
+        {
+            // Arrange
+            int[,] maMatrix = null;
+            int n = 0;
+
+            // Act
+            int[,] output = Matrix.Divide(maMatrix, n);
+
+            // Assert
+            Assert.AreEqual(null, output);
+        }
+    }
+}
---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>This is the Matrix class within MyMath namespace.</summary>
+    public class Matrix
+    {
+        /// <summary>Returns a new matrix containing divided elements.</summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix is null)
+            {
+                return null;
+            }
+            int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            
+            try
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        newMatrix[i, j] = matrix[i, j] / num;
+                    }
+                }
+
+                return newMatrix;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            
+            catch(NullReferenceException)
+            {
+                return null;
+            }
+        }
+    }
+}
