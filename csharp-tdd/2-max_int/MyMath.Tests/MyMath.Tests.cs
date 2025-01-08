71ed6a7f2b178b26b31d873fb5cf01bb5dd20555
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-08 12:12:26 +0100

    2. Max integer

---
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    /// <summary>Class for testing Operations.Max function.</summary>
+    [TestFixture]
+    public class MaxTests
+    {
+        /// <summary>Test when list contains positives values.</summary>
+        [Test]
+        public void TestListOfPositiveValues()
+        {
+            // Arrange
+            List<int> nums = new List<int> { 3, 8, 1 };
+
+            // Act
+            int output = Operations.Max(nums);
+
+            // Assert
+            Assert.AreEqual(8, output);
+        }
+
+        /// <summary>Test when the list is empty.</summary>
+        [Test]
+        public void TestListEmpty()
+        {
+            // Arrange
+            List<int> nums = new List<int>();
+
+            // Act
+            int output = Operations.Max(nums);
+
+            // Assert
+            Assert.AreEqual(0, output);
+        }
+
+        /// <summary>Test when the list contains negative values.</summary>
+        [Test]
+        public void TestListOfNegativeValues()
+        {
+            // Arrange
+            List<int> nums = new List<int>() { -3, -8, -1};
+
+            // Act
+            int output = Operations.Max(nums);
+
+            // Assert
+            Assert.AreEqual(-1, output);
+        }
+
+        /// <summary>Test when the list contains one value.</summary>
+        [Test]
+        public void TestListWithOneValue()
+        {
+            // Arrange
+            List<int> nums = new List<int>() { 1 };
+
+            // Act
+            int output = Operations.Max(nums);
+
+            // Assert
+            Assert.AreEqual(1, output);
+        }
+
+        /// <summary>Test when the list contains negative and positive values.</summary>
+        [Test]
+        public void TestListOfNegativeAndPositiveValues()
+        {
+            // Arrange
+            List<int> nums = new List<int>() { -3, -8, 1};
+
+            // Act
+            int output = Operations.Max(nums);
+
+            // Assert
+            Assert.AreEqual(1, output);
+        }
+    }
+}
---
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>This is the Operations class within MyMath namespace.</summary>
+    public class Operations
+    {
+        /// <summary>Returns the max integer in a list of integers.</summary>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+            {
+                return 0;
+            }
+            
+            int returnValue = nums[0];
+            
+            foreach (int i in nums)
+            {
+                if (i > returnValue)
+                {
+                    returnValue = i;
+                }
+            }
+            return returnValue;
+        }
+    }
+}
