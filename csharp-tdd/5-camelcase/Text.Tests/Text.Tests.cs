065a81f4b41196b00d4b9eda8f87d6a160849278
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-09 15:32:22 +0100

    5. camelCase

---
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Text;
+
+namespace Text.Tests
+{
+    /// <summary>Class for testing Str.CamelCase function.</summary>
+    [TestFixture]
+    public class CamelCaseTests
+    {
+        /// <summary>Test when empty string.</summary>
+        [Test]
+        public void TestEmptyString()
+        {
+            // Arrange
+            string s = String.Empty;
+
+            // Act
+            int output = Str.CamelCase(s);
+
+            // Assert
+            Assert.AreEqual(0, output);
+        }
+
+        /// <summary>Test when one letter.</summary>
+        [Test]
+        public void TestOneLetter()
+        {
+            // Arrange
+            string s = "a";
+
+            // Act
+            int output = Str.CamelCase(s);
+
+            // Assert
+            Assert.AreEqual(1, output);
+        }
+
+        /// <summary>Test when several words.</summary>
+        [Test]
+        public void TestSeveralWords()
+        {
+            // Arrange
+            string s = "abc Defg Hij";
+
+            // Act
+            int output = Str.CamelCase(s);
+
+            // Assert
+            Assert.AreEqual(3, output);
+        }
+
+    }
+}
---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text
+{
+    /// <summary>This is the Str class within Text namespace.</summary>
+    public class Str
+    {
+        /// <summary>Return how many words are in a string.</summary>
+        public static int CamelCase(string s)
+        {
+            int nbWords = 0;
+
+            if (s.Length > 0)
+            {
+                nbWords = 1;
+                 byte[] ASCIIValues = Encoding.ASCII.GetBytes(s);
+                foreach (Byte b in ASCIIValues)
+                {
+                    if (b >= 65 && b <= 90)
+                    {
+                        nbWords += 1;
+                    }
+                }
+            }
+
+            return nbWords;
+
+        }
+    }
+}
