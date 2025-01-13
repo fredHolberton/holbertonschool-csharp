8fa3a5ddb0441c0f6cbde3fa0506f492de123385
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-13 09:47:21 +0100

    1. For instance

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>This is an public class Obj.</summary>
+public class Obj
+{
+    /// <summary>Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, Array,
+    /// otherwise return False.
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return typeof(Array).IsInstanceOfType(obj);
+    }
+}
