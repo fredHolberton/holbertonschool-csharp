0f797af9b4594bdc12f7b4438382792bcba84f59
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-13 12:35:12 +0100

    5. Basic shapes

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>This is a public class Shape.</summary>
+public class Shape
+{
+    /// <summary>Function Area not implemented.</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException ("Area() is not implemented");
+    }
+}
