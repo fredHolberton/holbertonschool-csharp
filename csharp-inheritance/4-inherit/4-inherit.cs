cc78afaee5b980fe3eb3ee627cd200110503bea5
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-13 12:22:22 +0100

    4. It is not a lesson in classes and inheritance without a Dog

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>This is an public empty class Animal.</summary>
+public class Animal
+{
+    
+}
+
+/// <summary>This is an public empty class Dog that inherits from empty class Animal.</summary>
+public class Dog : Animal
+{
+
+}
