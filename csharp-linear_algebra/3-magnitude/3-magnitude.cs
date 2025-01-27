35e40d5abbae36d2f2d7c20fe8046276acb6fa80
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-27 12:01:14 +0100

    Task 3 Fix 2

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -7,11 +7,17 @@ public class VectorMath
     /// </summary>
     public static double Magnitude(double[] vector)
     {
+        if ((vector == NULL) || (vector.Length == 0))
+            return -1;
+        if (vector.Length < 2 || vector.Length > 3)
+            return -1;
+            
         double result = 0;
         foreach (double v in vector)
         {
-            result += v * 2;
+            result += v * v;
         }
-        return Math.Sqrt(result);
+        result = Math.Sqrt(result);
+        return Math.Round(result, 2);
     }
 }
