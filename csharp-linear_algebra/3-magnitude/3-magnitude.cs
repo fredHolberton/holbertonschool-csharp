56f0cfe4898f1f69be5cec48db135b30def1d0ff
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-27 11:33:13 +0100

    3. Magnitude #2

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>Public class VectorMath for manipulating vectors.</summary>
+public class VectorMath
+{
+    /// <summary>calculates and returns the length of a given vector.
+    /// </summary>
+    public static double Magnitude(double[] vector)
+    {
+        double result = 0;
+        foreach (double v in vector)
+        {
+            result += v * 2;
+        }
+        return Math.Sqrt(result);
+    }
+}
