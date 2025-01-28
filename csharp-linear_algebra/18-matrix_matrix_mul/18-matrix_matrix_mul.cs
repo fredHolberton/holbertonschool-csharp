93e03f3a7ddf869452f597b2115d0076ccc30bc3
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-28 10:10:15 +0100

    Task 18 Fix 13

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -11,20 +11,16 @@ public class MatrixMath
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
         if (matrix1.GetLength(1) != matrix2.GetLength(0))
-            return new double[,]{ {-1} };
+            return new double[,] { {-1} };
 
-        int rowsMatrix1 = matrix1.GetLength(0);
-        int colsMatrix1 = matrix1.GetLength(1);
-        int rowsMatrix2 = matrix2.GetLength(0);
-        int colsMatrix2 = matrix2.GetLength(1);
-        double[,] result = new double[rowsMatrix1, colsMatrix2];
+        double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
 
-        for (int i = 0; i < rowsMatrix1; i++)
+        for (int i = 0; i < matrix1.GetLength(0); i++)
         {
-            for (int j = 0; j < colsMatrix2; j++)
+            for (int j = 0; j < matrix2.GetLength(1); j++)
             {
                 result[i, j] = 0;
-                for (int k = 0; k < colsMatrix1; k++)
+                for (int k = 0; k < matrix1.GetLength(1); k++)
                 {
                     result[i, j] = result[i, j] + (matrix1[i, k] * matrix2[k, j]);
                 }
