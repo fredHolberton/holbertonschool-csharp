6455a7c2ec4b8250468e0e9e14caa33c7a4c69c4
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-28 15:52:40 +0100

    22. Matrix shear #1

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// <summary>
+/// Public class MatrixMath for manipulating matrix.
+/// </summary>
+public class MatrixMath
+{
+    /// <summary>
+    ///  shears a square 2D matrix by a given shear factor and returns the resulting matrix.
+    /// </summary>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] { {-1} };
+
+        if (direction != 'x' && direction != 'y')
+            return new double[,] { {-1} };
+
+        double[,] result = new double[,] { {0, 0}, {0, 0} };
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (direction == 'x')
+            {
+                result[i, 0] = matrix[i, 0] + (matrix[i, 1] * factor);
+                result[i, 1] = matrix[i, 1];
+            }                  
+            else
+            {
+                result[i, 0] = matrix[i, 0];
+                result[i, 1] = matrix[i, 1] + (matrix[i, 0] * factor);
+            }
+        }
+
+        return result;
+    }
+}
