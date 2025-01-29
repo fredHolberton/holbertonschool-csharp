a4c4cfdc5a391fb58ca0001ddf7d4d0fbe19b27d
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-29 10:14:16 +0100

    Task 26 Fix 4

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -10,7 +10,7 @@ public class MatrixMath
     /// </summary>
     public static double Determinant(double[,] matrix)
     {
-         if ((matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) || (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3) || (matrix.GetLength(0) != matrix.GetLength(1)))
+         if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3 ||matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3 || matrix.GetLength(0) != matrix.GetLength(1))
             return -1;
 
         double result;
