9486eafddeaf349a767ce9ff46cb8d3bb900d84c
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-03-25 09:44:51 +0100

    0. Abstract thinking

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// This class is abstract and can't be instanciated.
+/// </summary>
+abstract class Base
+{
+    /// <summary>String name property.</summary>
+    public string name;
+
+    /// <summary>Overrides Base method ToString to print information about name.</summary>
+    public override string ToString()
+    {
+        return name + " is a string";
+    }
+}
---
+++ csharp-interfaces/README.md
@@ -0,0 +1,1 @@
+Contains all task files or project : C# - Interfaces
