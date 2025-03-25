55dd37c8d826b2fd119a20666af9dc3174048866
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-03-25 16:47:40 +0100

    2. Escape room

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+/// <summary>
+/// Interface to interact.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>Interact method to be implemented.</summary>
+    void Interact();
+}
+
+/// <summary>
+/// Interface to break.
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>Durability property to be implemented.</summary>
+    int durability {get; set;}
+
+    /// <summary>Break method to be implemented.</summary>
+    void Break();
+}
+
+/// <summary>
+/// Interface to Collect.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>Boolean property isCollected to be implemented.</summary>
+    bool isCollected {get; set;}
+
+    /// <summary>Collect method to be implemented.</summary>
+    void Collect();
+}
+
+/// <summary>
+/// This class is abstract and can't be instanciated.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>String name property.</summary>
+    public string name {get; set;}
+
+    /// <summary>
+    /// Overrides Base method ToString to print information about name.
+    /// </summary>
+    public override string ToString()
+    {
+        return name + " is a " + this.GetType();
+    }
+}
+
+/// <summary>
+/// Door class that inherits Base and implements IInteractive.
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>Constructor that sets the value of name</summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    /// <summary>Implementation of Interact method of IInteractive</summary>
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the {0} It's locked.", this.name);
+    }
+
+}
+
