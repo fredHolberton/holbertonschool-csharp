95fe4b3c96bc8af41583365a773768835f607a22
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-04-03 17:17:48 +0200

    0. Base knowledge

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <summary>This is an public class that all other classes will inherit from.</summary>
+    public class BaseClass
+    {
+        /// <summary>Gets or sets the id of the object.</summary>
+        public string id { get; set; };
+
+        /// <summary>Gets or sets the created date of the object.</summary>
+        public DateTime date_created { get; set; };
+
+        /// <summary>Gets or sets the updated date of the object.</summary>
+        public dateTime date_updated { get; set; };
+
+    }
+}
---
+++ csharp-text_based_interface/README.md
@@ -0,0 +1,1 @@
+Contains task files of project: C# - Text-based Interface
